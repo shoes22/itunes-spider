ded18fa13f74c266c47ea700dc566bc99dacc880
montekaka <josh.chen.jia@gmail.com>
2020-10-18 22:29:34 -0700

    adds handle delete csv file in the tmp

--- server/models/itunes.js
+++ server/models/itunes.js
@@ -1,20 +1,29 @@
 'use strict';
 const scapers = require('../../libs');
+const fs = require('fs')
 const path = require('path');
 
 const parseAll = async () => {
-  const scaper = new scapers.iTunesScaper();
-  // scaper._getInfoFromiTunesApi('1069277113');
+  // determine if the file exist
+  const filePath = '../../tmp/itunes.csv'
+  
+  try {
+    fs.unlinkSync(path.resolve(__dirname, filePath))
+  } catch(err) {
+    //console.log(err)
+  }
 
+  fs.closeSync(fs.openSync(path.resolve(__dirname, filePath), 'w'))
+
+  const scaper = new scapers.iTunesScaper();  
   const csvFile = new scapers.CsvFile({
-    path: path.resolve(__dirname, '../../tmp/itunes.csv'),
+    path: path.resolve(__dirname, filePath),
     headers: ['id', 'link','feedUrl', 'trackName', 'author', 'trackCount', 'primaryGenreName', 'genres', 'releaseDate', 'email', 'language', 'firstReleaseDate'],
   })
 
   scaper.csvFile = csvFile;
-  await scaper.parsePopularPodcasts()
-  // await scaper.parse();
-  // console.log({podcastsCount})
+  await scaper.parsePopularPodcasts();
+  
 }
 
 module.exports = {
